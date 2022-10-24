564e3a7cc077735a938f65655b6a5dac92c423b5
Arjun Raj Jain <arjun@selfguard.xyz>
2022-10-24 00:21:47 -0400

    update

--- notifications.js
+++ notifications.js
@@ -5,17 +5,21 @@ let API_Key = process.argv[2]; // specify your api key
 let address = process.argv[3];
 let email = process.argv[4];
 let phone = process.argv[5];
-let sg = new SelfGuard(API_Key);
+let sg = new SelfGuard(API_Key,null,null,'http://localhost:8080');
 
+//6fda5947-5983-4492-b45f-33e71bbf460b
+
+console.log({API_Key});
 (async () => {
-
-  let addresses = await sg.getProfiles();
-  console.log(addresses);
-  let val = await sg.updateProfile(address,{email,phone});
-  console.log(val);
+  let group = await sg.createNotificationGroup({collection_name:"Hello2",contract_address:"hello2"})
+  // console.log({group});
+  // let val = await sg.updateProfile({address,value:{email,phone},collection_name:"Hello"});
+  // let addresses = await sg.getProfiles({collection_name:"Hello"});
+  // console.log(addresses);
+  // console.log(val);
 
   // let put = await sg.sendEmail({address,from:"test@selfguard.xyz", fromName:"testFromName", replyTo:"test@selfguard.xyz", replyToName:"testReplyToName", subject:"testSubject", html:"testContent"});
   // console.log({put});
-  // let put2 = await sg.sendSMS({address,text:'test'});
+  // let put2 = await sg.sendBulkSMS({collection_name:"Hello",text:'test'});
   // console.log({put2});
 })();
